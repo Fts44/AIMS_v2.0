6b5ffe2e5a96808a3779b11643b8fadcfde2670b
Fts44 <calmajoseph7@gmail.com>
2023-02-12 01:32:56 +0800

    Patient Covid Vaxx and Ins

--- resources/views/Patient/CovidVaccinationInsurance.blade.php
+++ resources/views/Patient/CovidVaccinationInsurance.blade.php
@@ -243,7 +243,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modal_title">Upload Files</h5>
+                <h5 class="modal-title" id="modal_title">Upload File</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="form_file_uploads" action="{{ route('Patient.COVIDFileUploads.Insert') }}">
